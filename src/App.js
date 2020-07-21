df0d13fd1486953c797ec6d652b1c3cf327d6199
cmsanner <craftbloggerfozzie@gmail.com>
2020-07-21 14:52:25 -0500

    change App to a class, start passing state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList';
+import SearchBox from './SearchBox';
 import {embroideryInfo} from './embroiderdata';
 
-const App = () =>{
-    return(
-        <CardList dataArray={embroideryInfo}/>
-    )
+class App extends Component {
+    constructor(){
+        super();
+        this.state ={
+            dataArray: embroideryInfo,
+            searchField: '' 
+        }
+    }
+    render(){
+        return(
+            <div className='tc'>
+                <h1>Embroidery Catalog</h1>
+                <SearchBox />
+                <CardList dataArray={this.state.dataArray}/>
+            </div>
+        )
+    }
+    
 }
 
 export default App;
